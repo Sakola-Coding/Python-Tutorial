b16f994ddb8ac99acb3c58417fd1c46a08a9d989
rintoprie <rintoprie@users.noreply.github.com>
2020-07-15 12:33:11 +0700

    Create chatbot0.py

---
+++ chatbot0.py
@@ -0,0 +1,14 @@
+# Menggunakan IDE trinket.io
+
+print("ask something")
+while True:
+  say = input()
+  if say == "halo":
+    print("halo juga")
+  elif say == "siapa namamu":
+    print("namaku chatbot")
+  elif say == "kamu tinggal di mana":
+    print("jakarta")
+  else:
+    print("maaf aku tidak paham pertanyaanmu")
+  
