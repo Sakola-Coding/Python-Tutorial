aa980ab694fece0ed547e698a2556ba6ef563b9b
rintoprie <rintoprie@users.noreply.github.com>
2020-07-15 13:33:11 +0700

    Create chatbot1.py

---
+++ chatbot1.py
@@ -0,0 +1,34 @@
+# Menggunakan IDE trinket.io
+# Chatbot level 1
+# Author: @rintoprie
+
+print("Silakan bertanya")
+while True:
+  say = input()
+  if say == "halo":
+    print("halo juga")
+  elif say == "siapa namamu":
+    print("namaku chatbot")
+  elif say == "kamu tinggal di mana":
+    print("jakarta")
+  elif say == "berapa 1+1":
+    print("2")
+  elif say == "berapa 2+2":
+    print("14")
+  elif say == "berapa 7x2":
+    print("14")
+  elif say == "apa nama ibukota jawa timur":
+    print("surabaya")
+  elif say == "apa makanan khas surabaya":
+    print("rujak cingur")
+  elif say == "siapa nama presiden indonesia":
+    print("pak jokowi")
+  elif say == "tahun berapa indonesia merdeka":
+    print("1945")
+  elif say == "tahun berapa terjadi perang diponegoro":
+    print("1825-1830")
+  elif say == "apa yg dimaksud dengan kambium":
+    print("Kambium adalah lapisan jaringan meristematik pada tumbuhan yang sel-selnya aktif membelah dan bertanggung jawab atas pertumbuhan sekunder tumbuhan. Kambium ditemukan pada batang dan akar.")
+  else:
+    print("maaf aku tidak paham pertanyaanmu")
+  
